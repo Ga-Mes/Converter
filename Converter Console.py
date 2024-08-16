669f19753675b7dd9bcaa89a289e25cbe0bde7fc
Ga_Mes <90312011+Ga-Mes@users.noreply.github.com>
2024-08-16 20:50:46 +1000

    Create Converter Console.py

---
+++ Converter Console.py
@@ -0,0 +1,36 @@
+import pygame
+import pyvidplayer2
+
+width, height = 80, 24
+
+pygame.init()
+screen = pygame.display.set_mode((width, height), pygame.HIDDEN)
+
+while True:
+    screen.fill((0, 0, 0))
+    pygame.display.update()
+
+    try:
+        video = pyvidplayer2.Video(input('File: '))
+    except Exception as e:
+        print(e)
+        continue
+
+    try:
+        f = open(f'{video.name}.tcvc', 'xb')
+    except Exception as e:
+        print(e)
+        continue
+
+    video.resize((width, height))
+    video.mute()
+
+    while video.active:
+        video.draw(screen, (0, 0), force_draw=True)
+        pygame.display.update()
+        for y in range(24):
+            for x in range(0, 80, 2):
+                f.write(bytes([*screen.get_at((x, y))[:3], *screen.get_at((x + 1, y))[:3]]))
+    f.close()
+
+pygame.quit()
